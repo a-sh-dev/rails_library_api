a83a8cb55d348f67e25b21de390a395bc6c16bd3
a-sh <iam.angie@ymail.com>
2021-11-17 21:14:13 +1100

    User initial testing

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    username { "user1" }
+    email { "user1@email.com" }
+    password { "password" }
+    password_confirmation { "password" }
+  end
+end
